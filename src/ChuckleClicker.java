83b45f0ba22f3dcfa4d311c8c764a9c8bb54ce83
pigchick3n <league@WTS-iMac-11.attlocal.net>
2017-10-14 15:53:33 -0700

    Fortune Cookie; Chuckle Clicker (IP)

---
+++ src/ChuckleClicker.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class ChuckleClicker {
+	public static void main(String[] args) {
+
+	}
+
+	public void makeButtons() {
+		JOptionPane.showMessageDialog(null, ":O");
+
+	}
+}
--- src/FortuneCookie.java
+++ src/FortuneCookie.java
@@ -1,4 +1,7 @@
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
 import javax.swing.JButton;
 /*
 Fortune Cookie recipe: Create a button that gives one of five random fortunes when clicked.
@@ -13,18 +16,51 @@ Add an action listener to the button (auto fix the errors!!). When the button is
 
 Generate a random number between 0 and 4. Depending on the random number, show on of five fortunes.
  */
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-public class FortuneCookie {
+public class FortuneCookie implements ActionListener {
+	JButton button;
+
 	public static void main(String[] args) {
-		
+		new FortuneCookie().showButton();
 	}
-	
-	public static void showButton() {
-		System.out.println("YO!");
+
+	public void showButton() {
+		System.out.println("Yo!");
 		JFrame frame = new JFrame();
 		frame.setVisible(true);
-		JButton button = new JButton();
+		button = new JButton();
+		button.setText("Click me to get your fortune!");
 		frame.add(button);
 		button.addActionListener(this);
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent action) {
+		// TODO Auto-generated method stub
+		if (action.getSource() == button) {
+			Random random = new Random();
+			int rand = random.nextInt(6);
+			System.out.println(rand);
+			if (rand == 0) {
+				JOptionPane.showMessageDialog(null,
+						"You will live a short life and die sufferabley. That is not a word just to let you know. Good luck in life. :o");
+			} else if (rand == 1) {
+				JOptionPane.showMessageDialog(null, "HI...");
+			} else if (rand == 2) {
+				JOptionPane.showMessageDialog(null, "Hellow!");
+			} else if (rand == 3) {
+				JOptionPane.showMessageDialog(null,
+						"Maybe you will live a good life? Maybe you won't. Who knows... I'M NOT A FORTUNE TELLER!!!");
+			} else if (rand == 4) {
+				JOptionPane.showMessageDialog(null, ":o");
+			} else if (rand == 5) {
+				JOptionPane.showMessageDialog(null,
+						"You broke the game, idiot. >:P You suck with technology. YOu weRe pRobaBly nOt boRn In geNerAtiOn Z.");
+			}
+		}
+	}
 }
+// the the the the the the the the the the the the the the the the the the the
+// the the the the the the the the the the the the the the the the the the the
