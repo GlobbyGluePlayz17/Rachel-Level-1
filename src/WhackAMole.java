50cd560d3f6be4b1309922acc8ca0b93f4d4c04e
League <league@WTS8.attlocal.net>
2018-02-03 15:53:19 -0800

    WhackAMole
    I did not fail and I did not not have fun.

--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -1,7 +1,10 @@
+import java.applet.AudioClip;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -12,6 +15,10 @@ public class WhackAMole implements ActionListener{
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton mole = new JButton("Mole");
+	
+	Date timeAtStart = new Date();
+	static int molesWacked = 0;
+	static int fails = 0;
 	
 	public void drawButtons(int random) {
 		frame.add(panel);
@@ -41,9 +48,32 @@ public class WhackAMole implements ActionListener{
 	
 	private void endGame(Date timeAtStart, int molesWhacked) {
 		Date timeAtEnd = new Date();
-		JOptionPane.showMessageDialog(null, "Your whack rate is " + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked) + " moles per second.");
+		if (molesWacked == 10) {
+			JOptionPane.showMessageDialog(null, "Your whack rate is " + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked) + " moles per second.");
+			JOptionPane.showMessageDialog(null, "End Game.");
+		}
+		
+		if (fails == 1) {
+			speak("You're a dork. You suck. Try Again.");
+		}
+		
+		if (fails == 2) {
+			speak("You're an idiot.");
+		}
+		
+		if (fails == 3) {
+			speak("You are a moron.");
+		}
+		
+		if (fails == 4) {
+			speak("You are a complete waste of atoms. U even hue-mon?");
+		}
+		
+		if (fails == 5) {
+			speak("You should have never existed. Do you even have a brain?!?");
+		}
 	}
-
+	
 	public static void main(String[] args) {
 		int rand = new Random().nextInt(24);
 		new WhackAMole().drawButtons(rand);
@@ -53,9 +83,21 @@ public class WhackAMole implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		if (e.getSource().equals(mole)) {
-			speak("You are a wonderful, yet mole hitting human being. Good job!");
+			speak("You hit the mole. Now do it again.");
+			playSound("WhackAMole.wav");
+			frame.dispose();
+			int rand = new Random().nextInt(24);
+			new WhackAMole().drawButtons(rand);
+			molesWacked++;
+			endGame(timeAtStart, molesWacked);
 		} else {
-			speak("You're a dork. You suck. Boo.");
+			fails++;
+			endGame(timeAtStart, molesWacked);
 		}
+	}
+	
+	private void playSound(String fileName) {
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+		sound.play();
 	}
 }
