bdaffc813fcb5732a1035f5a22a6aafbd4da459d
sakuyaflower <league@WTS10.attlocal.net>
2018-03-24 15:54:05 -0700

    Sample Test - Riddle

---
+++ src/Riddle.java
@@ -0,0 +1,21 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Riddle implements ActionListener{
+	public static void main(String[] args) {
+		Riddle r = new Riddle();
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
