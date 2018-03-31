4351223e5b678aa5203fad1a232b40dc2735f58c
GlobbyGluePlayz17 <league@WTS14.attlocal.net>
2018-03-31 11:41:31 -0700

    Binary Converter (IP)

---
+++ src/BinaryConverter.java
@@ -0,0 +1,66 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.regex.Pattern;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class BinaryConverter implements ActionListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JTextField userinput = new JTextField();
+	JTextField answer = new JTextField();
+	JButton button = new JButton("convert");
+
+	public static void main(String[] args) {
+		BinaryConverter b1 = new BinaryConverter();
+		b1.createUI();
+	}
+
+	public void createUI() {
+		frame.setVisible(true);
+		frame.setTitle("Binary Converter");
+		frame.setSize(300, 65);
+		frame.add(panel);
+		panel.add(userinput);
+		panel.add(button);
+		panel.add(answer);
+		userinput.addActionListener(this);
+	}
+
+	int convert(String binary) {
+		if (binary.length() != 8) {
+			JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+			return 0;
+		}
+		if (!Pattern.matches("\2", binary)) {
+			JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+			return 0;
+		}
+		try {
+			int asciiValue = Integer.parseInt(binary, 2);
+			return asciiValue;
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+			// question.setText("");
+			return 0;
+		}
+	}
+
+	String converttostring(String binary) {
+		int asciiValue = Integer.parseInt(binary, 2);
+		char theLetter = (char) asciiValue;
+		return "" + theLetter;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource().equals(button)) {
+			int output = convert(userinput.getText());
+		}
+	}
+}
--- src/Riddle.java
+++ src/Riddle.java
@@ -1,21 +1,20 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+public class Riddle implements ActionListener {
 
-public class Riddle implements ActionListener{
 	public static void main(String[] args) {
 		Riddle r = new Riddle();
-		
+
+	}
+
+	public void createUI() {
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
