f06dd2649864198a89fba1714f4594360873a1a7
GlobbyGluePlayz17 <league@iMac.attlocal.net>
2018-04-14 11:39:52 -0700

    Magic Box (IP)

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,4 +1,121 @@
+	import java.applet.AudioClip;
+import java.awt.Dimension;
+	import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+	import java.io.File;
+	import java.io.IOException;
+import java.net.URL;
 
-public class MagicBox {
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+	import javax.swing.SwingUtilities;
 
-}
+	public class MagicBox extends JPanel implements Runnable, MouseListener{
+
+		/*
+		 * Work together as a TEAM of 2 or 3 to make this project. We are going to hide secrets within the magic box. 
+		 * When the user clicks on a secret place, stuff will happen.
+		 * 
+		 * 1. Make the frame respond to mouse clicks.
+		 * 
+		 * 2. When the mouse is clicked, use the Media Palace (bit.ly/media-palace) to play sounds, show images or speak.
+		 * 
+		 * 3. backgroundImage.getRGB(keyEvent.getX(), keyEvent.getY()) will give you the color of the current pixel.
+		 */
+		JFrame frame = new JFrame("The Magic Box contains many secrets...");
+		JPanel panel = new JPanel();
+		JLabel boxLabelWithImage;
+		
+		BufferedImage backgroundImage;
+
+		public static void main(String[] args) throws Exception {
+			SwingUtilities.invokeLater(new MagicBox());
+		}
+
+		@Override
+		public void run() {
+			try {
+				loadBackgroundImage();
+				createUI();
+			} catch (Exception w) {
+				System.err.println(w.getMessage());
+			}
+		}
+
+		private void createUI() {
+			frame.add(panel);
+			panel.add(boxLabelWithImage);
+			setPreferredSize(new Dimension(backgroundImage.getWidth(), backgroundImage.getHeight()));
+			frame.pack();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+		}
+
+		private void loadBackgroundImage() throws Exception {
+			String imageFile = "magic-box.png";
+			boxLabelWithImage = loadImageFromWithinProject(imageFile);
+			boxLabelWithImage.addMouseListener(this);
+			
+			try {
+				backgroundImage = ImageIO.read(new File(imageFile));
+			} catch (IOException e) {
+				throw new Exception("Could not load image: " + imageFile);
+			}
+		}
+ 
+		public JLabel loadImageFromWithinProject(String openbox) {
+			URL imageURL = getClass().getResource(openbox);
+			Icon icon = new ImageIcon(imageURL);
+			return new JLabel(icon);
+		}
+		
+		@Override
+		public void paintComponent(Graphics g) {
+			g.drawImage(backgroundImage, 0, 0, null);
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("box");
+			playSound("explosion_magicbox");
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		private void playSound(String fileName) {
+		   	 AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+		   	 sound.play();
+		    }
+	}
